b8a2f53a5e79741b198dc81c43667245ee2bc2d7
Jidechi ukor <ukorjidechi@gmail.com>
2021-06-12 20:59:51 +0100

    updated dandle comma rule in eslint

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,7 +1,6 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then((vitals) => {
-      const { getCLS, getFID, getFCP, getLCP, getTTFB } = vitals;
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
