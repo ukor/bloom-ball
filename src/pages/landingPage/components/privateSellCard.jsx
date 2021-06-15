c69b38bb8282f58344ab574738200cca06629b62
Jidechi ukor <ukorjidechi@gmail.com>
2021-06-15 19:45:00 +0100

    feat: added  landing page hero component

---
+++ src/pages/landingPage/components/privateSellCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+import PrivateSellForm from "./privateSellForm";
+import { OUTTER_SPACE } from "../../../shared";
+
+function PrivateSellCard() {
+  return (
+    <Box flex={{ base: 1 }} marginX={OUTTER_SPACE}>
+      <Box boxShadow="md" paddingX="6" paddingY="10" rounded="md" bg="#141414" width="100%" height="max">
+        <PrivateSellForm />
+      </Box>
+    </Box>
+  );
+}
+
+export default PrivateSellCard;
---
+++ src/shared/const.js
@@ -0,0 +1,1 @@
+export const OUTTER_SPACE = { base: 10, sm: 2, md: 10, lg: 24, xl: 32 };
