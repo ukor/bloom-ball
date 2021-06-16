fed588082b209b993be73b2bc2711513fc322a4d
Jidechi ukor <ukorjidechi@gmail.com>
2021-06-16 09:32:18 +0100

    added  eventListener for  private  sell form

--- src/pages/landingPage/components/hero.jsx
+++ src/pages/landingPage/components/hero.jsx
@@ -7,18 +7,19 @@ import PrivateSellCard from "./privateSellCard";
 function Hero() {
   const [isLargeScreen] = useMediaQuery("(min-width:  48em)");
   return (
-    <Box marginY={{ base: 20, lg: 32 }}>
-      <Flex
-        direction={isLargeScreen ? "row" : "column"}
-        bgColor="black"
-        width="100%"
-        height={{ base: "50vh", lg: "50vh" }}
-        align="center"
-        justify="center"
-      >
-        <HeroHeader />
-        <PrivateSellCard />
-      </Flex>
+    <Box bgColor="black">
+      <Box paddingY={{ base: 32, lg: 12 }}>
+        <Flex
+          direction={isLargeScreen ? "row" : "column"}
+          width="100%"
+          height={{ base: "50vh", lg: "50vh" }}
+          align="center"
+          justify="center"
+        >
+          <HeroHeader />
+          <PrivateSellCard />
+        </Flex>
+      </Box>
     </Box>
   );
 }
